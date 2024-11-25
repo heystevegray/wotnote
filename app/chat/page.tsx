a510421b369d60cb428dda5d36a8efd912bfb1a0
Steve Gray <heystevegray.dev@gmail.com>
2024-11-25 18:52:37 -0500

    demo

---
+++ app/chat/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { useState } from "react"
+import { readStreamableValue } from "ai/rsc"
+
+import { generate } from "../actions"
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30
+
+export default function Home() {
+  const [generation, setGeneration] = useState<string>("")
+
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          const { object } = await generate("Messages during finals week.")
+
+          for await (const partialObject of readStreamableValue(object)) {
+            if (partialObject) {
+              setGeneration(
+                JSON.stringify(partialObject.notifications, null, 2)
+              )
+            }
+          }
+        }}
+      >
+        Ask
+      </button>
+
+      <pre>{generation}</pre>
+    </div>
+  )
+}
