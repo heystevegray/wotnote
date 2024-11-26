da07c2d1ee6c180e03ae3f38337ac58027166dd0
Steve Gray <heystevegray.dev@gmail.com>
2024-11-26 15:09:21 -0500

    .

--- app/actions.ts
+++ app/actions.ts
@@ -32,20 +32,22 @@ export async function generate({ key, input }: { key: Key; input: string }) {
         song: z.object({
           name: z.string(),
           artist: z.string(),
-          key: z.enum([
-            "c",
-            "c#",
-            "d",
-            "d#",
-            "e",
-            "f",
-            "f#",
-            "g",
-            "g#",
-            "a",
-            "a#",
-            "b",
-          ]),
+          key: z
+            .enum([
+              "c",
+              "c#",
+              "d",
+              "d#",
+              "e",
+              "f",
+              "f#",
+              "g",
+              "g#",
+              "a",
+              "a#",
+              "b",
+            ])
+            .describe("The key of the song."),
           // https://sdk.vercel.ai/providers/ai-sdk-providers/openai#structured-outputs
           error: z.string().nullable().describe("Error message if any."),
           scale: z.enum([
@@ -68,43 +70,51 @@ export async function generate({ key, input }: { key: Key; input: string }) {
                     .describe(
                       "Unique uuid identifier for the chord, example f837ed0d-d2f0-4fb7-9739-4e213b6d8d88."
                     ),
-                  scaleDegree: z.number(),
+                  scaleDegree: z
+                    .number()
+                    .describe("The scale degree of the chord."),
                   lyrics: z
                     .string()
                     .describe("The lyrics that map to this chord in the song"),
-                  key: z.enum([
-                    "c",
-                    "c#",
-                    "d",
-                    "d#",
-                    "e",
-                    "f",
-                    "f#",
-                    "g",
-                    "g#",
-                    "a",
-                    "a#",
-                    "b",
-                  ]),
-                  notes: z.array(
-                    z.object({
-                      code: z.number().describe("MIDI note code."),
-                      key: z.enum([
-                        "c",
-                        "c#",
-                        "d",
-                        "d#",
-                        "e",
-                        "f",
-                        "f#",
-                        "g",
-                        "g#",
-                        "a",
-                        "a#",
-                        "b",
-                      ]),
-                    })
-                  ),
+                  key: z
+                    .enum([
+                      "c",
+                      "c#",
+                      "d",
+                      "d#",
+                      "e",
+                      "f",
+                      "f#",
+                      "g",
+                      "g#",
+                      "a",
+                      "a#",
+                      "b",
+                    ])
+                    .describe(
+                      "The root note of the current chord, example A, C, E"
+                    ),
+                  notes: z
+                    .array(
+                      z.object({
+                        code: z.number().describe("MIDI note code."),
+                        key: z.enum([
+                          "c",
+                          "c#",
+                          "d",
+                          "d#",
+                          "e",
+                          "f",
+                          "f#",
+                          "g",
+                          "g#",
+                          "a",
+                          "a#",
+                          "b",
+                        ]),
+                      })
+                    )
+                    .describe("The notes that make up this chrod."),
                 })
               ),
             })
