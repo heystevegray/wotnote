9916ed551226040a9039c891f8254c7971a5442c
Steve Gray <heystevegray.dev@gmail.com>
2025-02-09 13:39:46 -0500

    .

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -31,10 +31,10 @@ const Play = () => {
   return (
     <div className="flex h-[calc(100svh-64px)] flex-col">
       <Container className="relative flex-1 flex-col text-center">
-        <div className="absolute right-0 top-0 p-4 text-end">
+        <div className="absolute left-0 top-0 p-4 text-start">
           <Detail
             label="Alias"
-            value={midiConfig.chords.details?.aliases.slice(0, 1)[0]}
+            value={midiConfig.chords.details?.aliases.join(", ")}
           />
           <Detail
             label="Notes"
@@ -51,6 +51,7 @@ const Play = () => {
           <Detail label="Tonic" value={midiConfig.chords.details?.tonic} />
           <Detail label="Type" value={midiConfig.chords.details?.type} />
         </div>
+        {/* {JSON.stringify(midiConfig.chords.activeNotes, null, 2)} */}
         <h2
           className={cn(
             "flex h-full flex-col items-center justify-center text-[10rem] text-muted-foreground"
@@ -62,10 +63,6 @@ const Play = () => {
           {midiConfig.chords.chordName}
           {/* // "Play at least two notes simultaneously..."} */}
         </h2>
-        {/* {JSON.stringify(midiConfig.chords.details, null, 2)} */}
-        {/* <pre className="text-muted-foreground">
-          {JSON.stringify(midiConfig.chords.details, null, 2)}
-        </pre> */}
       </Container>
       <div className="">
         <MidiKeyboard disableScale />
